25eaac16ea933b48876d378eeab979cb404ddb9c
villeneve <villeneveoliveira@gmail.com>
2026-08-11 15:04:02 -0300

    create sets to layers

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+import torch
+
+def setKaiming_(layer,alpha=0.0):
+    torch.nn.init.kaiming_normal_(layer.weight,alpha)
+    torch.nn.init.zeros_(layer.bias)
+    return layer
+
+def setXavier_(layer):
+    torch.nn.init.xavier_normal_(layer.weight)
+    torch.nn.init.zeros_(layer.bias)
+    return layer
