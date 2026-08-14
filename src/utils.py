f23e20dbcc646f7c7f4d8e31d20c6ffd2438360b
Villeneve Oliveira <villeneveoliveira@gmail.com>
2026-08-14 18:38:28 +0000

    cria a classe para o dataset

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import torch
 
+import os
+
 def setKaiming_(layer,alpha=0.0):
     torch.nn.init.kaiming_normal_(layer.weight,alpha)
     torch.nn.init.zeros_(layer.bias)
@@ -9,3 +11,10 @@ def setXavier_(layer):
     torch.nn.init.xavier_normal_(layer.weight)
     torch.nn.init.zeros_(layer.bias)
     return layer
+
+class Data(torch.utils.data.dataset):
+    def __init__(self):
+        super().__init__()
+    def __get
+    def forward(self, x:torch.Tensor):
+        return
