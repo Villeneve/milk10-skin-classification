2afaeb1cd83d679a5a56cc56eb52894454d6c4c5
Villeneve Oliveira <villeneveoliveira@gmail.com>
2026-08-15 21:23:08 +0000

    finalizada a classe de carregamento de imagens tratando indeterminado como maligno

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,13 @@
+#%%
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+
+from torchvision.transforms import v2
+
+import matplotlib.pyplot as plt
 
 import pandas as pd
+from tqdm.notebook import tqdm
 import os
 from PIL import Image
 
@@ -19,15 +25,51 @@ class ImageLoader(Dataset):
     def __init__(self, imgs_path:str, metadata_path:str, transforms=None):
         super().__init__()
         self.imgs_path = imgs_path
+        self.transforms = transforms
         self.df = pd.read_csv(metadata_path,)
         self.df = self.df.loc[:,["isic_id","image_type","diagnosis_1"]]
-        self.df["diagnosis_1"] = self.df["diagnosis_1"].apply(lambda x: 1 if x == "Malignant" else 0)
+        self.df["diagnosis_1"] = self.df["diagnosis_1"].apply(lambda x: 0 if x == "Benign" else 1)
         self.df["image_type"] = self.df["image_type"].apply(lambda x: 1 if x == "dermoscopic" else 0)
     
     def __getitem__(self, idx):
-        return Image.open(self.imgs_path+self.df.loc[idx,"isic_id"]+".jpg")
+        img = Image.open(self.imgs_path+self.df.iloc[idx]["isic_id"]+".jpg")
+        if self.transforms is not None:
+            img = self.transforms(img)
+        return img, *self.df.iloc[idx][["image_type","diagnosis_1"]].tolist()
     def __len__(self):
         return len(self.df)
 
+def printImage():
+    for img,imgType,label in tqdm(data):
+        img = img[0].permute(1,2,0).cpu().numpy()/2+1/2
+        plt.imshow(img)
+        plt.title(f'type={imgType.numpy()}; diag={label.numpy()}')
+        plt.show()
+        break
+    return
+
 if __name__ == "__main__":
-    data = ImageLoader(metadata_path="/storage/SSD1/.data/milk10k/metadata.csv")
+
+    data = ImageLoader(
+        imgs_path="/storage/SSD1/.data/milk10k/images/",
+        metadata_path="/storage/SSD1/.data/milk10k/metadata.csv",
+        transforms=v2.Compose([
+            v2.Resize(256),
+            v2.Pad(256//2,padding_mode='reflect'),
+            v2.RandomRotation(180,interpolation=v2.InterpolationMode.BILINEAR),
+            v2.CenterCrop((256,256)),
+            v2.ToImage(),
+            v2.ToDtype(torch.float32,scale=True),
+            v2.Normalize([.5]*3,[.5]*3)
+        ])
+    )
+    # data = DataLoader(
+    #     data,
+    #     batch_size=1,
+    #     shuffle=True,
+    #     num_workers=4,
+    #     drop_last=True,
+    #     pin_memory=True,
+    # )
+
+    print(data.df["diagnosis_1"].value_counts())
